8d0cb60bde7f66a85dd4c46d07bd21341ea5ce7e
Sandro de Gruijter <sandrodegruijter@gmail.com>
2022-02-22 15:01:33 +0100

    Wired up the navigation bar

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -8,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class NavigationComponent implements OnInit {
     currentFile: string = 'filename.csv';
 
+    options: string[] = ['file1', 'file2', 'file3'];
+
     constructor() {}
 
     ngOnInit(): void {}
+
+    onChange(selectedFile: string): void {}
 }
