655582c77fe3d3738f2316abfe165b759c4f30d0
Sandro de Gruijter <sandrodegruijter@gmail.com>
2022-02-22 14:34:35 +0100

    Initial commit

---
+++ src/app/settings/settings.component.ts
@@ -0,0 +1,17 @@
+import { Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-settings',
+    templateUrl: './settings.component.html',
+    styleUrls: ['./settings.component.scss'],
+})
+export class SettingsComponent implements OnInit {
+    constructor(private location: Location) {}
+
+    ngOnInit(): void {}
+
+    goBack(): void {
+        this.location.back();
+    }
+}
